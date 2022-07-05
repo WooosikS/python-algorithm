1e08d45d68ba4731e556d39ca75a224c0df93726
ZTeams <97518038+ZTeams@users.noreply.github.com>
2022-07-05 12:11:59 +0900

    1 sol

---
+++ 프로그래머스/10-JadenCase 문자열 만들기.py
@@ -0,0 +1,19 @@
+# 문자열의 첫글자는 대문자로, 나머지는 소문자로 변환한다.
+
+def solution(s):
+    strr = s.split(' ')
+    answer = ''
+
+    for i in range(len(strr)):
+        strr[i] = strr[i].capitalize()
+    answer = ' '.join(strr)
+    return answer
+
+
+
+
+s = "a a a a a a a a"
+s1 = "3people unFollowed me"
+s2 = "for the last week"
+
+print(solution(s))
--- 프로그래머스/test.py
+++
@@ -1,8 +0,0 @@
-def solution(x, n):
-    answer = []
-    for i in range(1, n+1):
-        answer.append(x * i)
-
-    return answer
-x, n = map(int, input().strip().split())
-print(solution(x, n))    
