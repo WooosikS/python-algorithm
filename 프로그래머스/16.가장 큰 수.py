a8a204bd10044959d2e20f38580fbd96906d51fe
ZTeams <97518038+ZTeams@users.noreply.github.com>
2022-07-08 19:41:27 +0900

    2022-07-08, 1 sol

---
+++ 프로그래머스/16.가장 큰 수.py
@@ -0,0 +1,13 @@
+# https://jokerldg.github.io/algorithm/2021/05/06/most-big-number.html
+
+
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x : x * 3, reverse=True)
+    return str(int(''.join(numbers)))
+
+
+numbers = [6, 10, 2]
+numbers2 = [3, 30, 34, 5, 9]
+
+print(solution(numbers2))
